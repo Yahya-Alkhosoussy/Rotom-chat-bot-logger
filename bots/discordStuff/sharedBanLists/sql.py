1e05d39f2a74aba9ae73825451fa5ad2494304cd
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-09-25 22:10:38 +0100

    Made the SQL for shared ban stuff

---
+++ bots/discordStuff/sharedBanLists/sql.py
@@ -0,0 +1,66 @@
+from aiosqlite import connect  # noqa
+from asyncio import run  # noqa
+from discord import Member, User
+from bots.discordStuff.sharedBanLists.utils import BannedMember, Servers, Statuses  # noqa
+from pathlib import Path
+
+base_db_dir = Path(__file__).parent.parent.parent.parent.parent / "Shark-Bot" / "databases"
+ban_list_db_path = base_db_dir / "shared ban list.db"
+
+
+async def add_to_ban_list(member: Member | User, ban_reason: str = ""):
+    async with connect(ban_list_db_path) as conn:
+        await conn.execute(
+            "INSERT OR IGNORE INTO ban_list (discord_id, discord_username, reason, status, initial_server_ban)"
+            " VALUES (?, ?, ?, ?, ?)",
+            (member.id, member.name, ban_reason, Statuses.BANNED.value, Servers.DAVEX.value),
+        )
+        await conn.commit()
+
+
+async def check_if_user_in_ban_list(member: Member | User):
+    async with connect(ban_list_db_path) as conn:
+        async with conn.execute("SELECT status FROM ban_list WHERE discord_id=?", (member.id,)) as cur:
+            result = await cur.fetchone()
+            if result is None:
+                return False
+            status = Statuses(result[0])
+            if status == Statuses.BANNED:
+                return True
+            return False
+
+
+async def get_banned_member(member: Member | User) -> tuple[str, str]:
+    async with connect(ban_list_db_path) as conn:
+        async with conn.execute("SELECT reason, initial_server_ban FROM ban_list WHERE discord_id=?", (member.id,)) as cur:
+            result = await cur.fetchone()
+            if result is None:
+                raise ValueError(f"Could not find user with id {member.id}")
+            return result[0], result[1]
+
+
+async def set_as_unbanned(member: Member | User):
+    _, init_server = await get_banned_member(member)
+    if init_server != Servers.DAVEX.value:
+        return
+    async with connect(ban_list_db_path) as conn:
+        await conn.execute("UPDATE ban_list SET status='unbanned' WHERE discord_id=?", (member.id,))
+        await conn.commit()
+
+
+async def get_banned_members() -> list[BannedMember]:
+    async with connect(ban_list_db_path) as conn:
+        async with conn.execute("SELECT discord_username, discord_id, reason, status, initial_server_ban FROM ban_list") as cur:
+            results = await cur.fetchall()
+            to_return: list[BannedMember] = []
+            for result in results:
+                to_return.append(
+                    BannedMember(
+                        name=result[0],
+                        id=result[1],
+                        reason=result[2],
+                        status=Statuses(result[3]),
+                        initial_server_ban=Servers(result[4]),
+                    )
+                )
+    return to_return
