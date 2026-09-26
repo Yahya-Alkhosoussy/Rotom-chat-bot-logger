e8f00df74533adb35b32092e6bc942f1114b101c
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-09-26 11:35:22 +0100

    Added a way for the bot to handle unbans

--- bots/discordStuff/sharedBanLists/banList.py
+++ bots/discordStuff/sharedBanLists/banList.py
@@ -4,13 +4,13 @@ import discord
 from discord.ext import commands, tasks
 
 from bots.discordStuff.sharedBanLists.sql import get_banned_members
-from bots.discordStuff.sharedBanLists.utils import BannedMember
+from bots.discordStuff.sharedBanLists.utils import BannedMember, Statuses
 
 
 class BanListChecker:
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.membersLookedAt: list[BannedMember] = []
+        self.membersLookedAt: dict[BannedMember, Statuses] = {}
         self._loop: tasks.Loop | None = None
 
     def is_running(self) -> bool:
@@ -49,11 +49,44 @@ class BanListChecker:
         await user.ban(reason=member.reason)
         await channel.send(f"{member.name} successfully banned.")
 
+    async def handle_unban_request(self, member: BannedMember, guild: discord.Guild):
+        channel = guild.get_channel(1516090780173860914)
+
+        if not isinstance(channel, discord.TextChannel):
+            return
+
+        await channel.send(
+            f"User {member.name} was unbanned from the original server they were banned in. "
+            "Should I attempt to unban them? (Within the next 2 days reply with `!confirm` to allow me to try to unban them"
+            f" or `!deny` to keep them banned.) As a reminder the reason they were banned is: {member.reason}",
+        )
+
+        def check(m: discord.Message):
+            return (m.content == "!confirm" or m.content == "!deny") and (m.channel == channel)
+
+        try:
+            reply = await self.bot.wait_for("message", check=check, timeout=2 * 24 * 60 * 60)  # 48 hours
+        except asyncio.TimeoutError:
+            await channel.send(f"Got no reply. {member.name} will not be unbanned.")
+            return
+        if reply.content == "!deny":
+            await channel.send(f"Request denied successfully. {member.name} will not be unbanned.")
+            return
+        await channel.send(f"Request to ban {member.name} acknowledged. Starting unban process...")
+        try:
+            await guild.unban(member)
+        except discord.NotFound as e:
+            await channel.send(f"Unban failed. Could not find user {member.name}. Error: {str(e)}")
+        except discord.Forbidden:
+            await channel.send("Unban failed. I do not have the proper permissions.")
+        except discord.HTTPException:
+            await channel.send("Something went wrong while unbanning, could not complete.")
+
     def start(self):
         async def _tick():
             banned_members = await get_banned_members()
             for member in banned_members:
-                if member in self.membersLookedAt:
+                if member in self.membersLookedAt and self.membersLookedAt[member] == member.status:
                     continue
 
                 guild = self.bot.get_guild(1017872414098608158)
@@ -61,9 +94,12 @@ class BanListChecker:
                     return
                 bans = [ban async for ban in guild.bans()]
                 banned_user_ids = [ban.user.id for ban in bans]
-                if member.id not in banned_user_ids:
+                if member.status == Statuses.UNBANNED and member.id in banned_user_ids:
+                    await self.handle_unban_request(member, guild)
+
+                if member.id not in banned_user_ids and member.status == Statuses.BANNED:
                     await self.handle_ban_request(member, guild)
-                self.membersLookedAt.append(member)
+                self.membersLookedAt[member] = member.status
 
         loop = tasks.loop(hours=8, reconnect=True)(_tick)
 
