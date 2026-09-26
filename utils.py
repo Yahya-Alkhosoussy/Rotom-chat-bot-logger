e197fa9bfe5605b90b949db47ffb406222b1a180
Yahya Alkhosoussy <97708352+Yahya-Alkhosoussy@users.noreply.github.com>
2026-09-26 12:39:08 +0100

    changed attachment uris to stay as a list

--- utils.py
+++ utils.py
@@ -83,4 +83,4 @@ class DiscordMessage:
     content: str
     time_sent: datetime
     time_deleted: datetime | None
-    attachment_uris: list[str] | str | None = None
+    attachment_uris: list[str] | None = None
